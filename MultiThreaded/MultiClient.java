215b3ef5468290b48fe9acdcce0e9196f7147a77
kamallochan2004 <kamallochandas2004@gmail.com>
2024-06-24 20:11:36 +0530

    initialized a multithreaded web server

---
+++ MultiThreaded/MultiClient.java
@@ -0,0 +1,5 @@
+public class MultiClient {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ MultiThreaded/MultiServer.java
@@ -0,0 +1,5 @@
+public class MultiServer {
+    public static void main(String[] args) {
+
+    }
+}
